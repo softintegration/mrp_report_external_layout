2207a24181549ef4f63ee587a8fd7c446ca99ccc
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-08-09 12:02:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Set the default layout in Mrp reports
+-------------------------------------
+By default in Odoo,the external layout is used in all reports,but mrp use the internal layout,this module allow to set the external layout. 
+
+
---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*- 
+
+
+{
+    'name': 'Set the default layout in Mrp reports',
+    'author': 'Soft-integration',
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'qweb': [],
+    'description': False,
+    'images': [],
+    'version': '1.0.1',
+    'category': 'Manufacturing/Manufacturing',
+    'demo': [],
+    'depends': ['mrp'],
+    'data': [
+        'report/mrp_production_templates.xml',
+    ],
+    'license': 'LGPL-3',
+}
